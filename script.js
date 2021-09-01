fffec4abda2144cb6b4f81f21defdb030e03df07
koji <koji@kojis-MacBook-Pro.local>
2021-09-01 13:09:40 -0400

    Update 9/01 - 1:09

--- script.js
+++ script.js
@@ -5,8 +5,6 @@
 // append on page
 // =============================================================
 // https://www.theaudiodb.com/523532/discography.php?s=
-
-
 // Step 1:Api 
 const DOMAIN = "https://www.theaudiodb.com/"
 const APIkey = "523532"
@@ -17,38 +15,47 @@ const ALBUMPIC = `${DOMAIN}api/v1/json/${APIkey}/searchalbum.php?s=`
 // when user enters a name input generate genAlbum will paste result in mainbox
 // description should populate underneath
 // get data and append info to main box and corresponding sections
-const searchB = document.querySelector("#search");
 
 let mainDescdiv = document.querySelector('.descDiv');
 
-let strAlbum = "";
-
+// =============================================================================
+// =========================artist name axios request===========================
 async function getAlbum(artist_name) {
     // console.log(artist_name == "drake");
     try {
         let res = await axios.get(`${BASE_URL}${artist_name}`);
-        console.log(res)
+        // console.log(res)
         let albums = res.data.album;
-        strAlbum = res.data.album[0].strAlbum
-        console.log(strAlbum)
+        console.log(albums)
+        getDesc(artist_name, albums)
+        // console.log(strAlbum)
         // console.log(res.data.album[0].strAlbum)
-        getDesc(albums)
     } catch (error) {
         console.log(error);
     }
     
 }
-
-function getDesc(albums) {
+// =============================================================================
+// ============================Descriptions=====================================
+async function getDesc(artist_name, albums) {
     let mainDescdiv = document.createElement('div');
+    let imgTagMain = document.createElement('img')
     let titleTag = document.createElement('h3'); 
+    let res3 = await axios(`${ALBUMPIC}${artist_name}&a=${albums[0].strAlbum}`)
+    imgTagMain.setAttribute('src', res3.data.album[0].strAlbumThumb);
+    mainDescdiv.appendChild(imgTagMain);
     let descDiv = document.querySelector('.descDiv')
     titleTag.innerText = albums[0].strAlbum
     mainDescdiv.appendChild(titleTag)
     descDiv.appendChild(mainDescdiv)
     // seperation
     for(let i = 1; i < 4; i++) {
+        let res2 = await axios(`${ALBUMPIC}${artist_name}&a=${albums[i].strAlbum}`)
+        console.log(res2)
+        let imgTag = document.createElement('img')
+        imgTag.setAttribute('src', res2.data.album[0].strAlbumThumb)
         let albumDiv = document.createElement('div');
+        albumDiv.appendChild(imgTag)
         let titleTag = document.createElement('p'); 
         titleTag.innerText = albums[i].strAlbum
         albumDiv.appendChild(titleTag)
@@ -56,49 +63,46 @@ function getDesc(albums) {
     }
     
 }
+// ==============================================================================
 // take the arguments of artist_name and str_album from previous function
 // use arguments to obtain array then create function that gets img and render to page 
+// =======================Search Button with Input being artist_name=============
+const searchB = document.querySelector("#search");
 
 const input = document.querySelector("input");
 
 searchB.addEventListener('click', () => {
     let artist_name = input.value; 
     getAlbum(artist_name);
-    getPICs(artist_name, strAlbum)
 })
 
-
-async function getPICs(artist_name, strAlbum) {
-    console.log(artist_name == "drake");
-    try {
-        // console.log(strAlbum)
-        let res2 = await axios.get(`${ALBUMPIC}${artist_name}&a=${strAlbum}`)
+// ==============================================================================
+// ==============================================================================
+async function getPICs(artist_name, albums) {
+    // console.log(artist_name == "drake");
+    
+    albums.forEach(async(album) => {
+        let res2 = await axios(`${ALBUMPIC}${artist_name}&a=${album.strAlbum}`)
         console.log(res2)
-        // let albumName = res.data.album.strAlbum;
-        // return albumName
-    } catch (error) {
-        console.log(error);
-    }
+        
+    })           
+    // let mainDescdiv = document.createElement('div');
+    // let titleTag = document.createElement('h3'); 
+    // let descDiv = document.querySelector('.descDiv')
+    // titleTag.innerText = albums[0].strAlbum
+    // mainDescdiv.appendChild(titleTag)
+    // descDiv.appendChild(mainDescdiv)
+    // // seperation
+    // for(let i = 1; i < 4; i++) {
+    //     let albumDiv = document.createElement('div');
+    //     let titleTag = document.createElement('p'); 
+    //     titleTag.innerText = albums[i].strAlbum
+    //     albumDiv.appendChild(titleTag)
+    //     mainDescdiv.appendChild(albumDiv)
+    
 }
-
-
+// ==============================================================================
 // create 4 elements
 // set 4 innerTexts
 // append each to respective div 
 // after function put inside async use argument as albums.strAlbum[i]
-
-
-
-
-
-
-// replacing the mainbox description
-// document.getElementById("main-descr").innerHTML = `${res.data.album.strAlbum}`;
-
-// replacing the 3 albums underneath main 
-// document.getElementById("desc-1").innerHTML = `${res.data.album.strAlbum}`;
-// document.getElementById("desc-2").innerHTML = `${res.data.album.strAlbum}`;
-// document.getElementById("desc-3").innerHTML = `${res.data.album.strAlbum}`;
-
-// ${artist_name}&a=${strAlbum}
-
